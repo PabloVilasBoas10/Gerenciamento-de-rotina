1d9dc6114dbe479704c3c5eb495dcd255c67f4e0
PabloVilasBoas10 <pablovilasboas02@gmail.com>
2023-10-13 20:52:39 -0300

    add função de login

--- assets/js/criar-conta.js
+++ assets/js/criar-conta.js
@@ -1,40 +1,39 @@
-export default function criarPessoa() {
-  document.addEventListener('DOMContentLoaded', function () {
-    const inputNome = document.querySelector('.input-nome')
-    const inputSenha = document.querySelector('.input-senha')
-    const inputConfirmarSenha = document.querySelector('.input-confirmar-senha')
-    const criar = document.querySelector('.btn-criar-conta')
+document.addEventListener('DOMContentLoaded', function () {
+  const inputNome = document.querySelector('.input-nome')
+  const inputSenha = document.querySelector('.input-senha')
+  const inputConfirmarSenha = document.querySelector('.input-confirmar-senha')
+  const criar = document.querySelector('.btn-criar-conta')
 
-    const loginPage = '../../index.html'
+  const loginPage = '../../index.html'
 
-    criar.addEventListener('click', (e) => {
-      e.preventDefault()
-      if (inputNome.value && inputSenha.value && inputConfirmarSenha) {
-        if (inputSenha.value != inputConfirmarSenha.value) {
-          alert("AS SENHAS NÃO SE COINCIDEM!")
+  criar.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (inputNome.value && inputSenha.value && inputConfirmarSenha) {
+      if (inputSenha.value != inputConfirmarSenha.value) {
+        alert("AS SENHAS NÃO SE COINCIDEM!")
+        return
+      }
+
+      for (let i = 0; i < localStorage.length; i++) {
+        const userKey = localStorage.key(i)
+        const passwordKey = localStorage.getItem(userKey)
+        // console.log(userKey, passwordKey)
+
+        if (userKey === inputNome.value.toUpperCase()) {
+          alert('USUARIO JÁ EXISTENTE, CRIE OUTRO!');
           return
         }
 
-        for (let i = 0; i < localStorage.length; i++) {
-          const userKey = localStorage.key(i)
-          const passwordKey = localStorage.getItem(userKey)
-          // console.log(userKey, passwordKey)
-
-          if (userKey === inputNome.value.toUpperCase()) {
-            alert('USUARIO JÁ EXISTENTE, CRIE OUTRO!');
-            return
-          }
-
-
-        }
-        localStorage.setItem(inputNome.value.toUpperCase(), inputSenha.value.toUpperCase())
-        setTimeout(() => {
-          window.location.href = loginPage
-        }, 1000)
 
       }
-    })
-  });
+      localStorage.setItem(inputNome.value.toUpperCase(), inputSenha.value.toUpperCase())
+      setTimeout(() => {
+        window.location.href = loginPage
+      }, 1000)
+
+    }
+  })
+});
 
 
-}
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,30 @@
-import criarPessoa from "./criar-conta.js";
-criarPessoa()
+document.addEventListener('DOMContentLoaded', function () {
+  const inputNome = document.querySelector('.input-nome')
+  const inputSenha = document.querySelector('.input-senha')
+  const logar = document.querySelector('.btn-logar')
+
+  const loginPage = '../html/area-do-usuario.html'
+
+  logar.addEventListener('click', (e) => {
+    e.preventDefault()
+    for (let i = 0; i < localStorage.length; i++) {
+      const userKey = localStorage.key(i)
+      const passwordKey = localStorage.getItem(userKey)
+      console.log(userKey, passwordKey)
+
+      if (userKey == inputNome.value.toUpperCase() && passwordKey == inputSenha.value.toUpperCase()) {
+        setTimeout(() => {
+          window.location.href = loginPage
+        }, 2000)
+        return
+      } else {
+        alert('Usuário ou Senha INCORRETAS!')
+        return
+      }
+    }
+
+    alert('Usuário Inexistente! Crie uma conta.')
+
+  })
+})
+
